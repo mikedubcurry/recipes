bcdda4c42cd7ab86d11e9de28b40234eb17fa94d
Michael Curry <michaelcurry95@gmail.com>
2021-12-19 16:09:01 -0500

    nav bar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,10 +2,16 @@ import Link from 'next/link';
 
 function Nav() {
 	return (
-		<nav>
-			<Link href="/">Home</Link>
-			<Link href="/recipes">All Recipes</Link>
-			<Link href="/api/random">Random</Link>
+		<nav className="bg-green-400 flex justify-evenly items-center h-10">
+			<span className="transition-all hover:scale-110 hover:text-slate-800">
+				<Link href="/">Home</Link>
+			</span>
+			<span className="transition-all hover:scale-110 hover:text-slate-800">
+				<Link href="/recipes">All Recipes</Link>
+			</span>
+			<span className="transition-all hover:scale-110 hover:text-slate-800">
+				<Link href="/api/random">Random</Link>
+			</span>
 		</nav>
 	);
 }
