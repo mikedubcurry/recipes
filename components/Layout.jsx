3c69ed8d0d9d9e2587779b34f82a308f6473c89f
Michael Curry <michaelcurry95@gmail.com>
2021-12-19 14:48:01 -0500

    layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,12 @@
+import Nav from './Nav';
+
 function Layout({ children }) {
-	return <main className="h-screen bg-slate-200">{children}</main>;
+	return (
+		<>
+			<Nav />
+			<main className="h-screen bg-slate-200">{children}</main>
+		</>
+	);
 }
 
 export default Layout;
