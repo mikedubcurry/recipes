336aef706222e733ec5da24162e4f1feb1847024
mikedubcurry <fender18295@gmail.com>
2021-12-21 09:48:51 -0500

    filteredRecipes

---
+++ components/FilteredRecipes.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+import RecipeCard from "./RecipeCard";
+
+function FilteredRecipes({ recipes }) {
+    const [filteredRecipes, setFilteredRecipes] = useState(recipes);
+    const [filterInput, setFilterInput] = useState("");
+
+    useEffect(() => {
+        if (filterInput) {
+            let filtered = recipes.filter((recipe) => {
+                return ingredientsInclude(recipe, filterInput);
+            });
+            setFilteredRecipes(filtered);
+        } else {
+            setFilteredRecipes(recipes);
+        }
+    }, [filterInput]);
+
+    return (
+        <>
+            <label htmlFor="filterRecipes"></label>
+            <input
+                autoComplete="off"
+                placeholder="Filter recipes"
+                id="filterRecipe"
+                name="filterRecipe"
+                type="text"
+                value={filterInput}
+                onChange={(e) => setFilterInput(e.target.value)}
+            />
+            {filteredRecipes.map((recipe) => (
+                <RecipeCard key={recipe.dishTitle} recipe={recipe} />
+            ))}
+        </>
+    );
+}
+
+export default FilteredRecipes;
+
+function ingredientsInclude(recipe, input) {
+    return recipe.ingredients.filter((ing) => {
+        return ing.ingredient.toLowerCase().trim().includes(input.toLowerCase().trim());
+    }).length;
+}
