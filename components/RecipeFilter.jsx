704f3f5a60a2d3757379012dd7f5eb6b16b52a16
mikedubcurry <fender18295@gmail.com>
2021-12-22 11:12:29 -0500

    filter bar to hide text input with onClickOutside hook.

--- components/RecipeFilter.jsx
+++ components/RecipeFilter.jsx
@@ -2,19 +2,27 @@ function RecipeFilter({ inputState }) {
     const [filterInput, setFilterInput] = inputState;
 
     return (
-        <div className="container w-1/4 flex-col md:flex-row items-center mb-4 flex justify-between">
-            <label htmlFor="filterRecipes">Filter:</label>
+        <div className="container w-full flex-col sm:flex-row sm:w-1/2 items-center mb-4 flex justify-center gap-2 md:gap-4">
+            <label className="self-start md:self-center" htmlFor="filterRecipes">
+                Filter:
+            </label>
             <input
-                className="bg-slate-400 px-8 py-2 text-black rounded-xl placeholder:text-gray-200"
+                className="bg-slate-400 w-full px-8 py-2 text-black placeholder:text-gray-200"
                 autoComplete="off"
-                placeholder="Filter recipes"
+                placeholder="Filter recipes..."
                 id="filterRecipes"
                 name="filterRecipe"
                 type="text"
                 value={filterInput}
                 onChange={(e) => setFilterInput(e.target.value)}
             />
-            <button onClick={() => setFilterInput("")}>Clear Filter</button>
+            <button
+                className="px-4 py-2 self-end md:w-full md:self-center rounded-lg bg-blue-300 disabled:bg-gray-400 disabled:text-gray-200"
+                disabled={filterInput.length === 0}
+                onClick={() => setFilterInput("")}
+            >
+                Clear Filter
+            </button>
         </div>
     );
 }
