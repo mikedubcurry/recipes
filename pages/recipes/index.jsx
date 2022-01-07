4241f6a93776febe84df097ee603cd2308510a53
mikedubcurry <fender18295@gmail.com>
2022-01-06 19:29:41 -0500

    lock?

--- pages/recipes/index.jsx
+++ pages/recipes/index.jsx
@@ -1,26 +1,25 @@
-import { supabase } from '../../utils/supabaseClient';
+import { supabase } from "../../utils/supabaseClient";
 
-import RecipeCard from '../../components/RecipeCard';
+import RecipeCard from "../../components/RecipeCard";
 
 function AllRecipes({ recipes }) {
-
-	return (
-		<ul className="container m-auto min-w-fill">
-			{recipes.map((recipe) => (
-				<li key={recipe.id}>
-					<RecipeCard recipe={recipe} />
-				</li>
-			))}
-		</ul>
-	);
+    return (
+        <ul className="container m-auto min-w-fill">
+            {recipes.map((recipe) => (
+                <li key={recipe.id}>
+                    <RecipeCard recipe={recipe} />
+                </li>
+            ))}
+        </ul>
+    );
 }
 
 export async function getStaticProps() {
     let { data: recipes, error } = await supabase.from("recipes").select("*, tags (id, tag)");
 
-	return {
-		props: { recipes },
-	};
+    return {
+        props: { recipes },
+    };
 }
 
 export default AllRecipes;
