0aecff2ab1ac2f0762bb67c568a1c71645fba9b9
mikedubcurry <fender18295@gmail.com>
2022-01-06 19:28:45 -0500

    display tags in RecipeCard

--- components/RecipeCard.jsx
+++ components/RecipeCard.jsx
@@ -2,13 +2,48 @@ import Link from "next/link";
 import PropTypes from "prop-types";
 
 function RecipeCard({ recipe }) {
+    const capitalize = (str) =>
+        str
+            .split(" ")
+            .map((wd) => wd.slice(0, 1).toUpperCase() + wd.slice(1, wd.length))
+            .join(" ");
+
+    recipe.tags = [...recipe.tags, ...recipe.tags].map((tag, i) => ({ ...tag, id: i }));
+
     return (
         <Link href={`/recipes/${recipe.slug}`}>
             <a className="w-full">
-            <article className="cursor-pointer w-full mb-4 transition-all duration-300 shadow-lg hover:shadow-xl bg-red-300 hover:bg-red-400 p-8 rounded-xl">
-                <header className="text-xl">{recipe.dishTitle}</header>
-                <p>{recipe.description}</p>
-            </article>
+                <article className="flex flex-col justify-between cursor-pointer w-full h-full min-h-[150px] transition-all duration-300 shadow-lg hover:shadow-xl bg-red-300 hover:bg-red-400 p-8 rounded-xl">
+                    <header className="text-xl">{capitalize(recipe.recipe_name)}</header>
+                    <p>{recipe.description}</p>
+                    <div className="">
+                        <span>
+                            Prep time: {recipe.prep_time}{" "}
+                            {recipe.prep_time > 1 || recipe.prep_time === 0 ? "minutes" : "minute"}
+                        </span>
+                    </div>
+                    <ul className="flex-wrap rounded flex justify-evenly gap-4">
+                        {recipe.tags.slice(0, 4).map(
+                            // truncate tags if longer than 4
+                            ({ tag, id }) => (
+                                <li
+                                    className="transition bg-orange-400 hover:bg-orange-300 px-4 py-2 rounded-xl"
+                                    key={id}
+                                >
+                                    {tag}
+                                </li>
+                            )
+                        )}
+                        {recipe.tags.length > 4 ? (
+                            <li
+                                className="transition text-gray-600 bg-orange-400 hover:bg-orange-300 px-4 py-2 rounded-xl"
+                                key="truncated"
+                            >
+                                {recipe.tags.length - 4} more tags
+                            </li>
+                        ) : null}
+                    </ul>
+                </article>
             </a>
         </Link>
     );
@@ -18,6 +53,7 @@ export default RecipeCard;
 
 RecipeCard.propTypes = {
     recipe: PropTypes.shape({
+        id: PropTypes.string,
         dishTitle: PropTypes.string,
         slug: PropTypes.string,
         description: PropTypes.string,
@@ -31,5 +67,11 @@ RecipeCard.propTypes = {
             })
         ),
         procedure: PropTypes.arrayOf(PropTypes.string),
+        tags: PropTypes.arrayOf(
+            PropTypes.shape({
+                tag: PropTypes.string,
+                id: PropTypes.string,
+            })
+        ),
     }),
 };
