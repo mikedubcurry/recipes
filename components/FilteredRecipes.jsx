50c6734cc186484e71a2970eb7dc12cdcd4efbb4
Michael Curry <michaelcurry95@gmail.com>
2022-01-08 16:55:26 -0500

    filter with tags too!

--- components/FilteredRecipes.jsx
+++ components/FilteredRecipes.jsx
@@ -11,19 +11,24 @@ function FilteredRecipes({ recipes, tags }) {
 	const [selectedTags, dispatch] = useReducer(tagsReducer, []);
 
 	useEffect(() => {
-		if (filterInput) {
+		if (filterInput || selectedTags.length) {
 			let filtered = recipes.filter((recipe) => {
 				return ingredientsInclude(recipe, filterInput) || dishTitleInclude(recipe, filterInput);
 			});
+
+			filtered = filtered.filter((recipe) => {
+				return tagsInclude(recipe, selectedTags);
+			});
+
 			setFilteredRecipes(filtered);
 		} else {
 			setFilteredRecipes(recipes);
 		}
-	}, [filterInput]);
+	}, [filterInput, selectedTags]);
 
 	return (
 		<>
-			<div className="col-start-1 col-end-3">
+			<div className="md:col-start-1 md:col-end-3">
 				<RecipeFilter tags={selectedTags} dispatch={dispatch} inputState={[filterInput, setFilterInput]} />
 				<TagList selectedTags={selectedTags} dispatch={dispatch} tags={tags} />
 			</div>
@@ -44,6 +49,17 @@ function ingredientsInclude(recipe, input) {
 
 function dishTitleInclude(recipe, input) {
 	return recipe.recipe_name.toLowerCase().trim().includes(input.toLowerCase().trim());
+}
+
+function tagsInclude(recipe, tags) {
+	let recipeTags = recipe.tags.map(({ id }) => id);
+
+    for (let id of tags) {
+		if (!recipeTags.includes(id)) {
+			return false;
+		}
+	}
+	return true;
 }
 
 function tagsReducer(tagIds, action) {
