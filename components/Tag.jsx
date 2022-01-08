a5a0e13ae80dbf54e8d58a908590e5fd3af10e03
Michael Curry <michaelcurry95@gmail.com>
2022-01-08 16:54:04 -0500

    no hover on card Tag

--- components/Tag.jsx
+++ components/Tag.jsx
@@ -8,7 +8,7 @@ function Tag({ tag, selected, dispatch }) {
         }
     }
 	if (selected === undefined) {
-		return <li className="transition max-w-fit bg-orange-400 hover:bg-orange-300 px-4 py-2 rounded-xl">{tag.tag}</li>;
+		return <li className="transition max-w-fit bg-orange-400 px-4 py-2 rounded-xl">{tag.tag}</li>;
 	} else {
 		return <li onClick={toggleSelect} className={`transition max-w-fit ${selected ? 'bg-orange-500' : 'bg-orange-400 hover:bg-orange-300'} px-4 py-2 rounded-xl`}>{tag.tag}</li>;
 	}
