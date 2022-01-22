76dd4baf0aeda45a150405d920e60d7b8dd49844
Michael Curry <michaelcurry95@gmail.com>
2022-01-22 18:21:44 -0500

    actionbuttonn is a button

--- components/ActionBtn.jsx
+++ components/ActionBtn.jsx
@@ -1,8 +1,9 @@
 function ActionButton({ btnText, handleClick, type }) {
 	return (
 		<button
+			type="button"
 			aria-label={btnText}
-			className={`px-4 py-2 ${
+			className={`px-2 py-1 ${
 				type === 'warn' ? 'bg-red-300' : type === 'success' ? 'bg-green-300' : 'bg-yellow-100'
 			} rounded-xl flex items-center`}
 			onClick={handleClick}
