1dd4f83d5ac006d6fa69bae1542b50b4d27fc812
Michael Curry <michaelcurry95@gmail.com>
2022-01-22 15:12:06 -0500

    tags wiggle on hover and editing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			keyframes: {
+				wiggle: {
+					'0%, 100%': { transform: 'rotate(-2deg)' },
+					'50%': { transform: 'rotate(2deg)' },
+				},
+			},
+      animation: {
+        'wiggle': 'wiggle .5s linear infinite',
+      }
+		},
+	},
+	plugins: [],
+};
