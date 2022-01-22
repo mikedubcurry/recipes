e4d9f31d001e6d0dc13b73c47fd79871e4b537c9
Michael Curry <michaelcurry95@gmail.com>
2022-01-22 09:57:26 -0500

    basic styles for recipe page

--- pages/recipes/[slug].jsx
+++ pages/recipes/[slug].jsx
@@ -2,27 +2,43 @@ import { supabase } from '../../utils/supabaseClient';
 
 function RecipePage({ recipe }) {
 	return (
-		<div>
-			<header>{recipe.dishTitle}</header>
-			<ul>
-				{recipe.ingredients.map((ing) => (
-					<li key={ing.ingredient}>
-						<p>{ing.ingredient}</p>
-						<span>
-							{ing.amount} {ing.unit}
-						</span>
-					</li>
-				))}
-			</ul>
-
-			<p>Instructions:</p>
-			<ol>
-				{recipe.procedure.map((step) => (
-					<li key={step}>
-						<p>{step}</p>
-					</li>
-				))}
-			</ol>
+		<div className="container m-auto py-4 p-auto px-2">
+			<header className="h-72">
+				<h1 className="text-3xl">{recipe.recipe_name}</h1>
+				<figure>
+					<img
+						src={recipe.img ? recipe.img : '/notfound.webp'}
+						alt={recipe.description}
+						className="max-h-52 w-full object-contain object-center"
+						width="200"
+						height="200"
+					/>
+					<figcaption className="text-slate-600">{recipe.description}</figcaption>
+				</figure>
+			</header>
+			<section className="bg-yellow-600 -mx-2 px-2 py-4">
+				<h2 className="text-xl">Ingredients</h2>
+				<ul className="list-disc">
+					{recipe.ingredients.map((ing) => (
+						<li className="list-disc ml-10" key={ing.ingredient}>
+							<p>{ing.ingredient}</p>
+							<span>
+								{ing.amount} {ing.unit}
+							</span>
+						</li>
+					))}
+				</ul>
+			</section>
+			<section className='bg-purple-600 -mx-2 px-2 py-4'>
+				<h2 className="text-xl">Instructions:</h2>
+				<ol>
+					{recipe.procedure.map((step) => (
+						<li className='list-decimal ml-10' key={step}>
+							<p>{step}</p>
+						</li>
+					))}
+				</ol>
+			</section>
 		</div>
 	);
 }
@@ -43,7 +59,7 @@ export async function getStaticProps(context) {
 export async function getStaticPaths() {
 	const { data: slugs, error } = await supabase.from('recipes').select('slug');
 
-	return { paths: slugs.map(slug => ({params: slug})), fallback: false };
+	return { paths: slugs.map((slug) => ({ params: slug })), fallback: false };
 }
 
 export default RecipePage;
