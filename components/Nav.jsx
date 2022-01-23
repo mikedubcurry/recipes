6e9e93e368b9b220853bdc648530ad517a76dbe4
Michael Curry <michaelcurry95@gmail.com>
2022-01-23 09:09:46 -0500

    no all recipes page, redirect to /

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,11 +8,6 @@ function Nav() {
 					Home
 				</span>
 			</Link>
-			<Link href="/recipes">
-				<span className="cursor-pointer flex items-center transition-all duration-300 bg-gradient-to-r h-full from-slate-300 via-gray-500/30 bg-yellow-500/30 hover:bg-blue-500/30 to-slate-300 px-6 font-semibold text-gray-700 hover:text-white">
-					All Recipes
-				</span>
-			</Link>
 			<Link href="/api/random">
 				<span className="cursor-pointer flex items-center transition-all duration-300 bg-gradient-to-r h-full from-slate-300 via-gray-500/30 bg-yellow-500/30 hover:bg-blue-500/30 to-slate-300 px-6 font-semibold text-gray-700 hover:text-white">
 					Random
