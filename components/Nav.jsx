f1eb2aa6f602fe36a21c6f812c4c922266e40401
Michael Curry <michaelcurry95@gmail.com>
2022-01-23 12:05:18 -0500

    better nav styles

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,22 +4,22 @@ function Nav() {
 	return (
 		<nav className="w-full h-10 flex items-center justify-evenly sticky top-0 bg-slate-300 z-100">
 			<Link passHref href="/">
-				<a className="h-full">
-					<span className="w-full justify-center cursor-pointer flex items-center transition-all duration-700 bg-gradient-to-r h-full from-slate-300 via-gray-500/30 bg-yellow-500/30 hover:bg-blue-500/30 to-slate-300 px-6 font-semibold text-gray-700 hover:text-white">
+				<a className="h-full transition underline underline-offset-4 decoration-1 decoration-slate-400 hover:decoration-slate-300 focus:decoration-slate-300">
+					<span className="w-full justify-center cursor-pointer flex items-center h-full px-6 font-semibold text-gray-700 ">
 						Home
 					</span>
 				</a>
 			</Link>
 			<Link passHref href="/api/random">
-				<a className="h-full">
-					<span className="w-full justify-center cursor-pointer flex items-center transition-all duration-700 bg-gradient-to-r h-full from-slate-300 via-gray-500/30 bg-yellow-500/30 hover:bg-blue-500/30 to-slate-300 px-6 font-semibold text-gray-700 hover:text-white">
+				<a className="h-full transition underline underline-offset-4 decoration-1 decoration-slate-400 hover:decoration-slate-300 focus:decoration-slate-300">
+					<span className="w-full justify-center cursor-pointer flex items-center h-full px-6 font-semibold text-gray-700 ">
 						Random
 					</span>
 				</a>
 			</Link>
 			<Link passHref href="/new">
-				<a className="h-full">
-					<span className="w-full justify-center cursor-pointer flex items-center transition-all duration-700 bg-gradient-to-r h-full from-slate-300 via-gray-500/30 bg-yellow-500/30 hover:bg-blue-500/30 to-slate-300 px-6 font-semibold text-gray-700 hover:text-white">
+				<a className="h-full transition underline underline-offset-4 decoration-1 decoration-slate-400 hover:decoration-slate-300 focus:decoration-slate-300">
+					<span className="w-full justify-center cursor-pointer flex items-center h-full px-6 font-semibold text-gray-700 ">
 						New
 					</span>
 				</a>
